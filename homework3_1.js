25c77b9ff54284392e664bf3cbceebc740dacc60
Takisha777 <55092730+Takisha777@users.noreply.github.com>
2020-08-04 12:09:13 +0300

    Fixed

--- homework3_1.js
+++ homework3_1.js
@@ -2,25 +2,24 @@
 //     Затем, удалить элементы из массива с 2 по 4 (включительно!). По мере изменений, выводить содержимое
 //     массива на страницу.
 //  ------------------------------------------------------------------------------------------------------
-
-let volumeUser = +prompt("Введите длинну масива:");
-if (!isNaN(volumeUser)) {
-    let arrUser = [];
-    while (arrUser.length < volumeUser) {
-        let varUser = +prompt("Введите числовой элемент массива,  осталось : " + (volumeUser-arrUser.length));
-        if (!isNaN(varUser)) {
-            arrUser.push(varUser);
-        } else {
-            alert("Введите числовое значение элемента масива");
+let volumeUser = "В";
+do {
+    let volumeUser = +prompt("Введите длинну масива:");
+    if (!isNaN(volumeUser)) {
+        let arrUser = [];
+        while (arrUser.length < volumeUser) {
+            let varUser = +prompt("Введите числовой элемент массива,  осталось : " + (volumeUser - arrUser.length));
+            if (!isNaN(varUser)) {
+                arrUser.push(varUser);
+            } else {
+                alert("Введите числовое значение элемента масива");
+            }
         }
+        document.writeln(arrUser.join(" | ") + " <br>");
+        arrUser.sort((a, b) => a - b);
+        document.writeln(arrUser.join(" | ") + "<br>");
+        arrUser.splice(1, 3);
+        document.writeln(arrUser.join(" | "));
+        break;
     }
-    document.writeln( arrUser.join(" | ")+" <br>");
-    arrUser.sort();
-    document.writeln(arrUser.join(" | ") + "<br>");
-    arrUser.splice(1,3);
-    document.writeln(arrUser.join(" | "));
-
-} else {
-    alert("Введите числовую длинну масива");
-    location.reload(true);
-}
+} while (isNaN(volumeUser));
--- homework3_2.js
+++ homework3_2.js
@@ -13,71 +13,51 @@
 let arr = [16,-37,54,-4,72,-56,47,4,-16,25,-37,46,4,-51,27,-63,4,-54,76,-4,12,-35,4,47];
 document.writeln(" Дан массив [16,-37,54,-4,72,-56,47,4,-16,25,-37,46,4,-51,27,-63,4,-54,76,-4,12,-35,4,47] <br> <br>");
 
-//     Найти сумму и количество положительных элементов.
-let varSum = 0, qnt = 0, sumEven = 0, even = 0, zero = 0, multi = 1;
+let varSum = 0, posQnt = 0, sumEven = 0, even = 0, zero = 0, multi = 1, minNum = arr[0], numMinPosition = 0, numMaxPosition = 0, maxNum = arr[0], arrMod = [];
 for (let i = 0; arr.length > i; i++) {
-    if (arr [i] !== 0) {
+    //     Найти сумму и количество положительных элементов.
+    if (arr[i] !== 0) {
         if (arr[i] > 0) {
-            varSum += arr [i];
-            qnt++;
-            multi *= arr [i];
-            if (arr [i] % 2 === 0) {
+            varSum += arr[i];
+            posQnt++;
+            multi *= arr[i];
+            if (arr[i] % 2 === 0) {
                 even++;
-                sumEven += arr [i];
+                sumEven += arr[i];
             }
         }
     } else {
         zero++;
     }
-}
-document.writeln("Сумма положительных элементов " + varSum + " и их количество " + qnt + "<br>");
-
-//     Найти минимальный элемент массива и его порядковый номер.
-let minNum = 0, numPosition = 0;
-for (let i = 0; arr.length > i; i++) {
-    if (arr[i] < minNum) {
+    //     Найти минимальный элемент массива и его порядковый номер.
+    if (arr[i] <= minNum) {
         minNum = arr[i];
-        numPosition = i+1;
+        numMinPosition = i + 1;
     }
-}
-document.writeln("Минимальный элемент массива " + minNum + " и его порядковый номер " + numPosition + "<br>");
 
-//     Найти максимальный элемент массива и его порядковый номер.
-let maxNum = 0;
-for (let i = 0; arr.length > i; i++) {
-    if (arr[i] > maxNum) {
+    //     Найти максимальный элемент массива и его порядковый номер.
+    if (arr[i] >= maxNum) {
         maxNum = arr[i];
-        numPosition = i+1;
+        numMaxPosition = i + 1;
     }
+    //     Найти наибольший среди элементов массива, остальные обнулить.
+        arrMod[i] = 0;
 }
-document.writeln("Максимальный элемент массива " + maxNum + " и его порядковый номер " + numPosition + "<br>");
-
-//     Определить количество отрицательных элементов.
-
+arrMod[numMaxPosition-1] = maxNum;
+document.writeln("Сумма положительных элементов " + varSum + " и их количество " + qnt + "<br>");
+document.writeln("Минимальный элемент массива " + minNum + " и его порядковый номер " + numMinPosition + "<br>");
+document.writeln("Максимальный элемент массива " + maxNum + " и его порядковый номер " + numMaxPosition + "<br>");
+    //     Определить количество отрицательных элементов.
 document.writeln("Количество отрицательных элементов " + (arr.length - qnt - zero) + "<br>");
-
-//     Найти количество нечетных положительных элементов.
+    //     Найти количество нечетных положительных элементов.
 document.writeln("Количество нечетных положительных элементов " + (qnt - even) + "<br>");
-
-//     Найти количество четных положительных элементов.
+    //     Найти количество четных положительных элементов.
 document.writeln("Количество четных положительных элементов " + even + "<br>");
-
-//     Найти сумму четных положительных элементов.
+    //     Найти сумму четных положительных элементов.
 document.writeln("Сумма четных положительных элементов " + sumEven + "<br>");
-
-//     Найти сумму нечетных положительных элементов.
-document.writeln("Сумма нечетных положительных элементов " + (varSum-sumEven) + "<br>");
-
-//     Найти произведение положительных элементов.
+    //     Найти сумму нечетных положительных элементов.
+document.writeln("Сумма нечетных положительных элементов " + (varSum - sumEven) + "<br>");
+    //     Найти произведение положительных элементов.
 document.writeln("Произведение положительных элементов " + multi + "<br>");
-
 //     Найти наибольший среди элементов массива, остальные обнулить.
-let arrMod = [];
-for (let i = 0; i < arr.length; i++) {
-    if ( arr [i] < maxNum) {
-        arrMod [i] = 0;
-    } else {
-        arrMod [i] = maxNum;
-    }
-}
 document.writeln( arrMod.join(" | "));
